b4e12be694f9813084b6aa1c5745bd513aec28af
PJ1016 <41801479+PJ1016@users.noreply.github.com>
2024-05-14 12:01:51 +0530

    update info

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,10 @@
 import React from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./conatiners/home";
-import Meghana from "./conatiners/home/Meghana";
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Home />,
-  },
-  {
-    path: "/Meghana",
-    element: <Meghana />,
   },
 ]);
 function App() {
